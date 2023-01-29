b1b079c082706854ad78bb8694652da422ca0cc0
Ilyusha <ilyushavavi@gmail.com>
2023-01-29 21:50:08 +0300

    social network layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
-import Header from './header';
-import Technologes from './technologes';
+import Header from './components/Header';
+import NavBar from './components/NavBar';
+import Profile from './components/Profile';
 
 const App = () => {
 	return (
-		<div>
+		<div className='app-wrapper'>
 			<Header />
-			<Technologes />
+			<NavBar />
+			<Profile />
 		</div>
 	);
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+const Header = () => {
+	return <header className='header'>
+			<img src="https://static.rfstat.com/renderforest/images/v2/landing-pics/logo_landing/Website/website_logo_7.png" />
+		</header>
+}
+export default Header;
